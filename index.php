29dd096a8344ae3369b78b4d0823dfd6c7f238c3
Valerie Jardin <valerie.jardin60@gmail.com>
2017-01-31 14:05:40 +0100

    Les nombres pairs ...

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
      for ($nombresImpairs = 0;
           $nombresImpairs < 300;
           $nombresImpairs++ )
+    {
+            if ($nombresImpairs%2 == 1)
+          {
+            echo ".";
+          }
+            else
           {
             echo "<p>$nombresImpairs</p>";
           }
-
+    }
 
  ?>
